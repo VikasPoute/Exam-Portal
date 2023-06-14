de2a7746d948b1ca4e206fd7671f7f4f59a5850c
Vikas Poute <vikaspoute@gmail.com>
2023-06-14 15:45:40 +0530

    changed material module

--- Frontend/exam-portal/src/app/app.module.ts
+++ Frontend/exam-portal/src/app/app.module.ts
@@ -7,21 +7,13 @@ import { AppComponent } from './app.component';
 import { BodyComponent } from './body/body.component';
 import { SidenavComponent } from './components/sidenav/sidenav.component';
 import { SublevelMenuComponent } from './components/sidenav/sublevel-menu.component';
-import { MatIconModule } from '@angular/material/icon';
 import { HomeComponent } from './pages/home/home.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { MatInputModule } from '@angular/material/input';
-import { MatButtonModule } from '@angular/material/button';
-import { MatFormFieldModule } from '@angular/material/form-field';
 import { SignupComponent } from './pages/signup/signup.component';
-import { MatCardModule } from '@angular/material/card';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { MatDividerModule } from '@angular/material/divider';
-import { MatDialogModule } from '@angular/material/dialog';
 import { HttpClientModule } from '@angular/common/http';
 import { LoginComponent } from './pages/login/login.component';
-import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { authInterceptorProvider } from './services/intercepter/auth.interceptor';
 import { UserDashboardComponent } from './pages/user/user-dashboard/user-dashboard.component';
 import { AuthGuardService } from './services/guard/auth-guard.service';
@@ -30,15 +22,11 @@ import { AdminGuard } from './services/guard/admin/admin-guard.guard';
 import { AuthGuard } from './services/guard/auth-guard.guard';
 import { ProfileComponent } from './pages/profile/profile.component';
 import { AdminComponent } from './pages/admin/admin/admin.component';
-import { MatTableModule } from '@angular/material/table';
 import { AddCategoryComponent } from './pages/admin/add-category/add-category.component';
-import { MatAccordion, MatExpansionModule } from '@angular/material/expansion';
-import { MatChipsModule } from '@angular/material/chips';
 import { ViewCategoryComponent } from './pages/admin/view-category/view-category.component';
 import { ViewQuizzesComponent } from './pages/admin/view-quizzes/view-quizzes.component';
-import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { AddQuizComponent } from './pages/admin/add-quiz/add-quiz.component';
-import { MatSelectModule } from '@angular/material/select';
+import { MaterialModule } from './material/material.module';
 
 @NgModule({
   declarations: [
@@ -62,23 +50,11 @@ import { MatSelectModule } from '@angular/material/select';
     BrowserModule,
     BrowserAnimationsModule,
     AppRoutingModule,
-    MatIconModule,
     FormsModule,
     ReactiveFormsModule,
-    MatInputModule,
-    MatButtonModule,
-    MatFormFieldModule,
-    MatCardModule,
     NgbModule,
-    MatDividerModule,
-    MatDialogModule,
     HttpClientModule,
-    MatSnackBarModule,
-    MatTableModule,
-    MatExpansionModule,
-    MatChipsModule,
-    MatSlideToggleModule,
-    MatSelectModule
+    MaterialModule
   ],
   providers: [authInterceptorProvider, UserGuard, AdminGuard, AuthGuard],
   bootstrap: [AppComponent],
